b36e89b31ca97e84a7b3ce5a44a5947eb63ce0b9
Quoc Dung Dang <dungthinhvn@gmail.com>
2019-02-16 18:03:52 +0700

    asdsa

--- resources/views/xpro/topic.blade.php
+++ resources/views/xpro/topic.blade.php
@@ -3,6 +3,19 @@
 
 @section('body')
 
+	<!-- Intro Section -->
+	<section class="inner-intro  padding bg-img1 overlay-dark light-color">
+		<div class="container">
+			<div class="row title">
+				<h1>Contact</h1>
+				<div class="page-breadcrumb">
+					<a>Home</a>/<span>Contact</span>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- End Intro Section -->
+
 
 
 @stop
