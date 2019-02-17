ea7cbc7e794ff75aa30d72888cfd8d0a0cd526cc
Quoc Dung Dang <dungthinhvn@gmail.com>
2019-02-17 11:25:41 +0700

    asdsa

--- resources/views/xpro/topic.blade.php
+++ resources/views/xpro/topic.blade.php
@@ -234,6 +234,18 @@
 						<li class="partner-logo border-b">
 							<img src="/xpro/assets/images/client-logo/1.png" alt="">
 						</li>
+						<li class="partner-logo border-b">
+							<img src="/xpro/assets/images/client-logo/2.png" alt="">
+						</li>
+						<li class="partner-logo border-b">
+							<img src="/xpro/assets/images/client-logo/3.png" alt="">
+						</li>
+						<li class="partner-logo border-b">
+							<img src="/xpro/assets/images/client-logo/4.png" alt="">
+						</li>
+						<li class="partner-logo border-b">
+							<img src="/xpro/assets/images/client-logo/5.png" alt="">
+						</li>
 					</ul>
 				</div>
 			</div>
